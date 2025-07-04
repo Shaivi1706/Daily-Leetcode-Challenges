8eff0e5cc7ed66fa8f46ae278a1a7cd289ccd090
Shaivi Jain <shaivijainn@gmail.com>
2025-07-04 20:20:03 +0530

    Create 4.cpp

---
+++ July 2025/4.cpp
@@ -0,0 +1,27 @@
+// LEETCODE 3307
+
+class Solution {
+public:
+    char kthCharacter(long long k, vector<int>& operations) {
+        int shift = 0;
+        vector<long long> lengths;
+        long long l = 1;
+
+        for (int op : operations) {
+            l *= 2;
+            lengths.push_back(l);
+            if (l >= k) break;
+        }
+
+        for (int i = lengths.size() - 1; i >= 0; --i) {
+            long long half = lengths[i] / 2;
+            int op = operations[i];
+            if (k > half) {
+                k -= half;
+                if (op == 1) shift++;
+            }
+        }
+
+        return (char)((('a' - 'a' + shift) % 26) + 'a');
+    }
+};
